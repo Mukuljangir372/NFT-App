df9719123c44845911d17d2b4af439a9289d5d77
mukul372 <mukul@scogo.in>
2022-04-30 17:46:11 +0530

    modules changes

--- buildSrc/src/main/java/BuildModules.kt
+++ buildSrc/src/main/java/BuildModules.kt
@@ -9,6 +9,4 @@ object BuildModules {
 
     //:commons
     private const val COMMONS = ":commons"
-    const val COMMONS_VIEWS = "$COMMONS:views"
-    const val COMMONS_MODELS = "$COMMONS:models"
 }
---
+++ commons/domain/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    id(BuildPlugins.ANDROID_LIBRARY)
+    id(BuildPlugins.KOTLIN_ANDROID_JETBRAINS)
+}
+
+android {
+    compileSdk = BuildAndroidConfig.COMPILE_SDK_VERSION
+
+    defaultConfig {
+        multiDexEnabled = true
+    }
+}
+
+dependencies {
+    //CORE --------------------------------------------------------------------------------------
+    implementation(Libs.CORE_KTX)
+    implementation(Libs.KOTLIN_STDLIB_JDK)
+}
